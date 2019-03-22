c0dfd7205502871a57ae95aa40802b9156af72a9
botiboti <botondferenczi98@gmail.com>
2019-03-22 02:28:50 +0200

    updated app list

--- home/botiboti/.nixpkgs/config.nix
+++ home/botiboti/.nixpkgs/config.nix
@@ -12,6 +12,7 @@
       paths = with pkgs; [
         binutils transmission-gtk networkmanager_dmenu
         tree volumeicon gcc w3m tmux atom chromium git
+        arandr mpv 
       ];
     };
   };
