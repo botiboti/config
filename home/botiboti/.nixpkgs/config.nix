066ffe7356784f29d5f94a10254d73c4a6ad99c5
botiboti <botondferenczi98@gmail.com>
2019-04-24 14:39:12 +0300

    elm

--- home/botiboti/.nixpkgs/config.nix
+++ home/botiboti/.nixpkgs/config.nix
@@ -12,7 +12,7 @@
       paths = with pkgs; [
         binutils transmission-gtk networkmanager_dmenu
         tree volumeicon gcc w3m tmux atom chromium git
-        arandr mpv libreoffice calibre
+        arandr mpv libreoffice calibre elmPackages.elm
       ];
     };
   };
