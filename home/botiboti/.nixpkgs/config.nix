91b4b402f781d352b56771c74f08a129b497d8df
Botond Ferenczi <botondferenczi98@gmail.com>
2020-11-24 22:37:13 +0000

    home-manager setup, cleanup

--- home/botiboti/.nixpkgs/config.nix
+++ home/botiboti/.nixpkgs/config.nix
@@ -1,23 +1,3 @@
 {
   allowUnfree = true;
-
-  chromium = {
-    # enablePepperFlash = true;
-    # enablePepperPDF = true;
-  };
-
-  packageOverrides = pkgs: rec {
-    apps = pkgs.buildEnv {
-      name = "apps";
-      paths = with pkgs; [
-        transmission-gtk networkmanager_dmenu
-        tree volumeicon gcc w3m tmux atom chromium git
-        arandr mpv libreoffice calibre elmPackages.elm
-        arduino xclip vscode elmPackages.elm-format
-        xdotool maim kodelife nodejs pavucontrol
-        blender rustup zoom-us discord steam librecad
-        simplescreenrecorder keepass teams pciutils vim
-      ];
-    };
-  };
 }
